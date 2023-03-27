73b969390fed2d99cdda186b4fd4f971fca6df84
Michael Panciroli <tinsheep@live.com>
2023-03-27 12:49:52 -0700

    Create Team from template and wait for success

--- bot/src/dist/httpTrigger.dev.js
+++ bot/src/dist/httpTrigger.dev.js
@@ -6,16 +6,18 @@ var _require = require("@microsoft/adaptivecards-tools"),
     AdaptiveCards = _require.AdaptiveCards;
 
 var _require2 = require("./internal/initialize"),
-    bot = _require2.bot; //try this:
-
+    bot = _require2.bot;
 
 var _require3 = require("@microsoft/teamsfx"),
     AppCredential = _require3.AppCredential,
-    createMicrosoftGraphClientWithCredential = _require3.createMicrosoftGraphClientWithCredential; // HTTP trigger to send notification. You need to add authentication / authorization for this API. Refer https://aka.ms/teamsfx-notification for more details.
+    createMicrosoftGraphClientWithCredential = _require3.createMicrosoftGraphClientWithCredential;
+
+var _require4 = require('@microsoft/microsoft-graph-client'),
+    ResponseType = _require4.ResponseType; // HTTP trigger to send notification. You need to add authentication / authorization for this API. Refer https://aka.ms/teamsfx-notification for more details.
 
 
 module.exports = function _callee(context, req) {
-  var _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, target, appAuthConfig, appCredential, graphClient, teamTemplate, team;
+  var _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, target, appAuthConfig, appCredential, graphClient, teamTemplate, team, location, teamStatus, checkStatusResponse;
 
   return regeneratorRuntime.async(function _callee$(_context) {
     while (1) {
@@ -34,7 +36,7 @@ module.exports = function _callee(context, req) {
 
         case 8:
           if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
-            _context.next = 22;
+            _context.next = 35;
             break;
           }
 
@@ -46,14 +48,13 @@ module.exports = function _callee(context, req) {
             tenantId: process.env.M365_TENANT_ID,
             clientSecret: process.env.M365_CLIENT_SECRET
           };
-          appCredential = new AppCredential(appAuthConfig); //const token = appCredential.getToken()
-
+          appCredential = new AppCredential(appAuthConfig);
           graphClient = createMicrosoftGraphClientWithCredential(appCredential); //create Incident response team from a teamsTemplate
 
           teamTemplate = {
             'template@odata.bind': 'https://graph.microsoft.com/v1.0/teamsTemplates(\'' + process.env.TEAMS_TEMPLATE_ID + '\')',
-            displayName: 'My Incident A',
-            description: 'My Incident A description',
+            displayName: 'My Incident 327-1',
+            description: 'My Incident 327-1 description',
             members: [{
               '@odata.type': '#microsoft.graph.aadUserConversationMember',
               roles: ['owner'],
@@ -61,11 +62,41 @@ module.exports = function _callee(context, req) {
             }]
           };
           _context.next = 16;
-          return regeneratorRuntime.awrap(graphClient.api('/teams').post(teamTemplate));
+          return regeneratorRuntime.awrap(graphClient.api('/teams').responseType(ResponseType.RAW).post(teamTemplate));
 
         case 16:
           team = _context.sent;
-          _context.next = 19;
+          console.log(team.headers.get('client-request-id')); // get the URL value where we can make the call to check if the asynchonous operation to create the Team is complete.
+          // this can take a minute or so
+
+          location = team.headers.get('Location');
+          teamStatus = "inProgress";
+
+        case 20:
+          if (!(teamStatus != "succeeded")) {
+            _context.next = 29;
+            break;
+          }
+
+          _context.next = 23;
+          return regeneratorRuntime.awrap(graphClient.api(location).get());
+
+        case 23:
+          checkStatusResponse = _context.sent;
+          teamStatus = checkStatusResponse.status;
+          _context.next = 27;
+          return regeneratorRuntime.awrap(new Promise(function (resolve) {
+            return setTimeout(resolve, 5000);
+          }));
+
+        case 27:
+          _context.next = 20;
+          break;
+
+        case 29:
+          console.log("Team created successfully!"); //********************************************************/
+
+          _context.next = 32;
           return regeneratorRuntime.awrap(target.sendAdaptiveCard(AdaptiveCards.declare(notificationTemplate).render({
             title: "New Incident Occurred!",
             appName: "Disaster Tech",
@@ -73,46 +104,46 @@ module.exports = function _callee(context, req) {
             notificationUrl: "https://m365x501367.sharepoint.com/sites/MyIncident4/Shared%20Documents/General/469308021927-5522126379-ticket.pdf"
           })));
 
-        case 19:
+        case 32:
           _iteratorNormalCompletion = true;
           _context.next = 8;
           break;
 
-        case 22:
-          _context.next = 28;
+        case 35:
+          _context.next = 41;
           break;
 
-        case 24:
-          _context.prev = 24;
+        case 37:
+          _context.prev = 37;
           _context.t1 = _context["catch"](3);
           _didIteratorError = true;
           _iteratorError = _context.t1;
 
-        case 28:
-          _context.prev = 28;
-          _context.prev = 29;
+        case 41:
+          _context.prev = 41;
+          _context.prev = 42;
 
           if (!_iteratorNormalCompletion && _iterator["return"] != null) {
             _iterator["return"]();
           }
 
-        case 31:
-          _context.prev = 31;
+        case 44:
+          _context.prev = 44;
 
           if (!_didIteratorError) {
-            _context.next = 34;
+            _context.next = 47;
             break;
           }
 
           throw _iteratorError;
 
-        case 34:
-          return _context.finish(31);
+        case 47:
+          return _context.finish(44);
 
-        case 35:
-          return _context.finish(28);
+        case 48:
+          return _context.finish(41);
 
-        case 36:
+        case 49:
           /****** To distinguish different target types ******/
 
           /** "Channel" means this bot is installed to a Team (default to notify General channel)
@@ -152,11 +183,11 @@ module.exports = function _callee(context, req) {
           **/
           context.res = {};
 
-        case 37:
+        case 50:
         case "end":
           return _context.stop();
       }
     }
-  }, null, null, [[3, 24, 28, 36], [29,, 31, 35]]);
+  }, null, null, [[3, 37, 41, 49], [42,, 44, 48]]);
 };
 //# sourceMappingURL=httpTrigger.dev.js.map
