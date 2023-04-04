86f9c365bf2d0b7c979bdc947b8d2ce80b1c57c2
Michael Panciroli <8007578+tinsheep@users.noreply.github.com>
2023-04-04 09:54:44 -0700

    updates to file upload but still not working

--- bot/src/dist/httpTrigger.dev.js
+++ bot/src/dist/httpTrigger.dev.js
@@ -17,133 +17,273 @@ var _require4 = require('@microsoft/microsoft-graph-client'),
 
 
 module.exports = function _callee(context, req) {
-  var _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, target, appAuthConfig, appCredential, graphClient, teamTemplate, team, location, teamStatus, checkStatusResponse;
+  var _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, target, _appAuthConfig, appCredential, graphClient, teamTemplate, team, location, teamId, teamStatus, checkStatusResponse, _ref, driveId, _generalChannelId, filePath, incidentReportUrl, getGeneralChannelDriveId, uploadFileToTeamsChannel;
 
-  return regeneratorRuntime.async(function _callee$(_context) {
+  return regeneratorRuntime.async(function _callee$(_context3) {
     while (1) {
-      switch (_context.prev = _context.next) {
+      switch (_context3.prev = _context3.next) {
         case 0:
+          uploadFileToTeamsChannel = function _ref3(driveId, teamId, graphClient, filePath) {
+            var fs, path, fileName, fileSize, fileContent, uploadSession, uploadUrl, maxChunkSize, start, end, fileSlice, bytesRead, _response, response, deepLink, uploadedFileUrl;
+
+            return regeneratorRuntime.async(function uploadFileToTeamsChannel$(_context2) {
+              while (1) {
+                switch (_context2.prev = _context2.next) {
+                  case 0:
+                    fs = require('fs');
+                    path = require('path');
+                    fileName = path.basename(filePath);
+                    fileSize = fs.statSync(filePath).size;
+                    fileContent = fs.createReadStream(filePath); //A Microsoft Graph API call to upload a local file to the Teams General channel
+
+                    _context2.prev = 5;
+                    _context2.next = 8;
+                    return regeneratorRuntime.awrap(graphClient.api("/drives/".concat(driveId, "/root:/General/").concat(fileName, ":/createUploadSession")).post({
+                      item: {
+                        '@microsoft.graph.conflictBehavior': 'rename',
+                        name: fileName
+                      }
+                    }));
+
+                  case 8:
+                    uploadSession = _context2.sent;
+                    uploadUrl = uploadSession.uploadUrl;
+                    maxChunkSize = 320 * 1024; // 320 KB
+
+                    start = 0;
+                    end = maxChunkSize;
+
+                  case 13:
+                    if (!(start < fileSize)) {
+                      _context2.next = 24;
+                      break;
+                    }
+
+                    if (fileSize - end < 0) {
+                      end = fileSize;
+                    }
+
+                    fileSlice = Buffer.alloc(maxChunkSize);
+                    bytesRead = fileContent.read(fileSlice, 0, maxChunkSize, start);
+                    _context2.next = 19;
+                    return regeneratorRuntime.awrap(fetch(uploadUrl, {
+                      method: 'PUT',
+                      headers: {
+                        'Content-Range': "bytes ".concat(start, "-").concat(start + bytesRead - 1, "/").concat(fileSize)
+                      },
+                      body: fileSlice.slice(0, bytesRead)
+                    }));
+
+                  case 19:
+                    _response = _context2.sent;
+                    start += bytesRead;
+                    end += maxChunkSize;
+                    _context2.next = 13;
+                    break;
+
+                  case 24:
+                    _context2.next = 26;
+                    return regeneratorRuntime.awrap(fetch(uploadUrl, {
+                      method: 'POST',
+                      headers: {
+                        'Content-Length': 0
+                      }
+                    }));
+
+                  case 26:
+                    response = _context2.sent;
+                    //Get the URL to the uploaded file
+                    deepLink = "https://teams.microsoft.com/l/file/".concat(encodeURIComponent(fileName), "/preview?groupId=").concat(teamId, "&tenantId=").concat(appAuthConfig.tenantId, "&channelId=").concat(generalChannelId);
+                    uploadedFileUrl = "https://teams.microsoft.com/_#/files/tab/".concat(teamId, "/").concat(driveId, "/").concat(encodeURIComponent(fileName));
+                    console.log("File uploaded successfully to ".concat(uploadedFileUrl));
+                    return _context2.abrupt("return", deepLink);
+
+                  case 33:
+                    _context2.prev = 33;
+                    _context2.t0 = _context2["catch"](5);
+                    console.error("Error uploading file: ".concat(_context2.t0));
+                    throw _context2.t0;
+
+                  case 37:
+                  case "end":
+                    return _context2.stop();
+                }
+              }
+            }, null, null, [[5, 33]]);
+          };
+
+          getGeneralChannelDriveId = function _ref2(teamId, client) {
+            var channels, generalChannelId, drive, driveId;
+            return regeneratorRuntime.async(function getGeneralChannelDriveId$(_context) {
+              while (1) {
+                switch (_context.prev = _context.next) {
+                  case 0:
+                    _context.next = 2;
+                    return regeneratorRuntime.awrap(client.api("/teams/".concat(teamId, "/channels")).filter("displayName eq 'General'").select('id').get());
+
+                  case 2:
+                    channels = _context.sent;
+                    generalChannelId = channels.value[0].id;
+                    _context.next = 6;
+                    return regeneratorRuntime.awrap(client.api("/teams/".concat(teamId, "/channels/").concat(generalChannelId, "/filesFolder")).get());
+
+                  case 6:
+                    drive = _context.sent;
+                    driveId = drive.parentReference.driveId;
+                    return _context.abrupt("return", {
+                      driveId: driveId,
+                      generalChannelId: generalChannelId
+                    });
+
+                  case 9:
+                  case "end":
+                    return _context.stop();
+                }
+              }
+            });
+          };
+
           _iteratorNormalCompletion = true;
           _didIteratorError = false;
           _iteratorError = undefined;
-          _context.prev = 3;
-          _context.next = 6;
+          _context3.prev = 5;
+          _context3.next = 8;
           return regeneratorRuntime.awrap(bot.notification.installations());
 
-        case 6:
-          _context.t0 = Symbol.iterator;
-          _iterator = _context.sent[_context.t0]();
-
         case 8:
+          _context3.t0 = Symbol.iterator;
+          _iterator = _context3.sent[_context3.t0]();
+
+        case 10:
           if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
-            _context.next = 35;
+            _context3.next = 50;
             break;
           }
 
           target = _step.value;
           //try this: **********************************************
-          appAuthConfig = {
+          _appAuthConfig = {
             authorityHost: process.env.M365_AUTHORITY_HOST,
             clientId: process.env.M365_CLIENT_ID,
             tenantId: process.env.M365_TENANT_ID,
             clientSecret: process.env.M365_CLIENT_SECRET
           };
-          appCredential = new AppCredential(appAuthConfig);
+          appCredential = new AppCredential(_appAuthConfig);
           graphClient = createMicrosoftGraphClientWithCredential(appCredential); //create Incident response team from a teamsTemplate
 
           teamTemplate = {
             'template@odata.bind': 'https://graph.microsoft.com/v1.0/teamsTemplates(\'' + process.env.TEAMS_TEMPLATE_ID + '\')',
-            displayName: 'My Incident 327-1',
-            description: 'My Incident 327-1 description',
+            displayName: 'My Incident 43-6',
+            description: 'My Incident 43-6 description',
             members: [{
               '@odata.type': '#microsoft.graph.aadUserConversationMember',
               roles: ['owner'],
               'user@odata.bind': 'https://graph.microsoft.com/v1.0/users/' + process.env.MOD_ID
             }]
           };
-          _context.next = 16;
+          _context3.next = 18;
           return regeneratorRuntime.awrap(graphClient.api('/teams').responseType(ResponseType.RAW).post(teamTemplate));
 
-        case 16:
-          team = _context.sent;
+        case 18:
+          team = _context3.sent;
           console.log(team.headers.get('client-request-id')); // get the URL value where we can make the call to check if the asynchonous operation to create the Team is complete.
-          // this can take a minute or so
+          // this can take a couple of minutes to fully complete.
 
-          location = team.headers.get('Location');
+          location = team.headers.get('Location'); // also get the teamId out of the location URL
+
+          teamId = location.match(/'([^']+)'/)[1];
           teamStatus = "inProgress";
 
-        case 20:
+        case 23:
           if (!(teamStatus != "succeeded")) {
-            _context.next = 29;
+            _context3.next = 32;
             break;
           }
 
-          _context.next = 23;
+          _context3.next = 26;
           return regeneratorRuntime.awrap(graphClient.api(location).get());
 
-        case 23:
-          checkStatusResponse = _context.sent;
+        case 26:
+          checkStatusResponse = _context3.sent;
           teamStatus = checkStatusResponse.status;
-          _context.next = 27;
+          _context3.next = 30;
           return regeneratorRuntime.awrap(new Promise(function (resolve) {
             return setTimeout(resolve, 5000);
           }));
 
-        case 27:
-          _context.next = 20;
+        case 30:
+          _context3.next = 23;
           break;
 
-        case 29:
-          console.log("Team created successfully!"); //********************************************************/
+        case 32:
+          console.log("Team created successfully!"); // I need the driveId so i can post the incident report to the channel
 
-          _context.next = 32;
+          _context3.next = 35;
+          return regeneratorRuntime.awrap(getGeneralChannelDriveId(teamId, graphClient));
+
+        case 35:
+          _ref = _context3.sent;
+          driveId = _ref.driveId;
+          _generalChannelId = _ref.generalChannelId;
+          console.log("The driveId of the General channel for team ".concat(teamId, " is ").concat(driveId));
+          console.log("The id of the General channel for team ".concat(teamId, " is ").concat(_generalChannelId));
+          filePath = 'C:\\OneNoteLocal\\Incident Report.pdf'; //upload the incident report to the general channel
+
+          _context3.next = 43;
+          return regeneratorRuntime.awrap(uploadFileToTeamsChannel(driveId, teamId, graphClient, filePath));
+
+        case 43:
+          incidentReportUrl = _context3.sent;
+          console.log("File uploaded to General channel: ".concat(incidentReportUrl)); //********************************************************/
+
+          _context3.next = 47;
           return regeneratorRuntime.awrap(target.sendAdaptiveCard(AdaptiveCards.declare(notificationTemplate).render({
             title: "New Incident Occurred!",
             appName: "Disaster Tech",
             description: "Welcome to the new incident team. Here is the Incident Action Plan:  ".concat(target.type),
-            notificationUrl: "https://m365x501367.sharepoint.com/sites/MyIncident4/Shared%20Documents/General/469308021927-5522126379-ticket.pdf"
+            notificationUrl: incidentReportUrl
           })));
 
-        case 32:
+        case 47:
           _iteratorNormalCompletion = true;
-          _context.next = 8;
+          _context3.next = 10;
           break;
 
-        case 35:
-          _context.next = 41;
+        case 50:
+          _context3.next = 56;
           break;
 
-        case 37:
-          _context.prev = 37;
-          _context.t1 = _context["catch"](3);
+        case 52:
+          _context3.prev = 52;
+          _context3.t1 = _context3["catch"](5);
           _didIteratorError = true;
-          _iteratorError = _context.t1;
+          _iteratorError = _context3.t1;
 
-        case 41:
-          _context.prev = 41;
-          _context.prev = 42;
+        case 56:
+          _context3.prev = 56;
+          _context3.prev = 57;
 
           if (!_iteratorNormalCompletion && _iterator["return"] != null) {
             _iterator["return"]();
           }
 
-        case 44:
-          _context.prev = 44;
+        case 59:
+          _context3.prev = 59;
 
           if (!_didIteratorError) {
-            _context.next = 47;
+            _context3.next = 62;
             break;
           }
 
           throw _iteratorError;
 
-        case 47:
-          return _context.finish(44);
+        case 62:
+          return _context3.finish(59);
 
-        case 48:
-          return _context.finish(41);
+        case 63:
+          return _context3.finish(56);
 
-        case 49:
+        case 64:
           /****** To distinguish different target types ******/
 
           /** "Channel" means this bot is installed to a Team (default to notify General channel)
@@ -183,11 +323,11 @@ module.exports = function _callee(context, req) {
           **/
           context.res = {};
 
-        case 50:
+        case 65:
         case "end":
-          return _context.stop();
+          return _context3.stop();
       }
     }
-  }, null, null, [[3, 37, 41, 49], [42,, 44, 48]]);
+  }, null, null, [[5, 52, 56, 64], [57,, 59, 63]]);
 };
 //# sourceMappingURL=httpTrigger.dev.js.map
--- bot/src/httpTrigger.js
+++ bot/src/httpTrigger.js
@@ -5,6 +5,7 @@ const { bot } = require("./internal/initialize");
 const { AppCredential, createMicrosoftGraphClientWithCredential } = require("@microsoft/teamsfx");
 const { ResponseType } = require('@microsoft/microsoft-graph-client');
 
+
 // HTTP trigger to send notification. You need to add authentication / authorization for this API. Refer https://aka.ms/teamsfx-notification for more details.
 module.exports = async function (context, req) {
   for (const target of await bot.notification.installations()) {
@@ -24,8 +25,8 @@ module.exports = async function (context, req) {
 
     const teamTemplate = {
       'template@odata.bind': 'https://graph.microsoft.com/v1.0/teamsTemplates(\'' + process.env.TEAMS_TEMPLATE_ID  +  '\')',
-      displayName: 'My Incident 327-1',
-      description: 'My Incident 327-1 description',
+      displayName: 'My Incident 43-6',
+      description: 'My Incident 43-6 description',
       members:[
           {
              '@odata.type': '#microsoft.graph.aadUserConversationMember',
@@ -43,9 +44,12 @@ module.exports = async function (context, req) {
         .post(teamTemplate);
     console.log(team.headers.get('client-request-id'));
 
+
     // get the URL value where we can make the call to check if the asynchonous operation to create the Team is complete.
-    // this can take a minute or so
-    const location = team.headers.get('Location')
+    // this can take a couple of minutes to fully complete.
+    const location = team.headers.get('Location');
+    // also get the teamId out of the location URL
+    const teamId = location.match(/'([^']+)'/)[1];
 
     let teamStatus = "inProgress";
     while (teamStatus != "succeeded") {
@@ -55,6 +59,17 @@ module.exports = async function (context, req) {
     }
     console.log("Team created successfully!");
 
+    // I need the driveId so i can post the incident report to the channel
+    const {driveId, generalChannelId} = await getGeneralChannelDriveId(teamId, graphClient);
+    console.log(`The driveId of the General channel for team ${teamId} is ${driveId}`);
+    console.log(`The id of the General channel for team ${teamId} is ${generalChannelId}`);
+
+    const filePath = 'C:\\OneNoteLocal\\Incident Report.pdf';
+
+    //upload the incident report to the general channel
+    const incidentReportUrl = await uploadFileToTeamsChannel(driveId, teamId, graphClient, filePath);
+    console.log(`File uploaded to General channel: ${incidentReportUrl}`);
+
 
     //********************************************************/
 
@@ -63,7 +78,7 @@ module.exports = async function (context, req) {
         title: "New Incident Occurred!",
         appName: "Disaster Tech",
         description: `Welcome to the new incident team. Here is the Incident Action Plan:  ${target.type}`,
-        notificationUrl: "https://m365x501367.sharepoint.com/sites/MyIncident4/Shared%20Documents/General/469308021927-5522126379-ticket.pdf",
+        notificationUrl: incidentReportUrl,
       })
     );
   }
@@ -106,4 +121,79 @@ module.exports = async function (context, req) {
   **/
 
   context.res = {};
+
+  async function getGeneralChannelDriveId(teamId, client) {
+    const channels = await client.api(`/teams/${teamId}/channels`).filter(`displayName eq 'General'`).select('id').get();
+    const generalChannelId = channels.value[0].id;
+    const drive = await client.api(`/teams/${teamId}/channels/${generalChannelId}/filesFolder`).get();
+    const driveId = drive.parentReference.driveId;
+    return {driveId, generalChannelId};
+  }
+
+
+  async function uploadFileToTeamsChannel(driveId, teamId, graphClient, filePath) {
+    const fs = require('fs');
+    const path = require('path');
+    const fileName = path.basename(filePath);
+    const fileSize = fs.statSync(filePath).size;
+    const fileContent = fs.createReadStream(filePath);
+
+    //A Microsoft Graph API call to upload a local file to the Teams General channel
+  
+    try {
+      const uploadSession = await graphClient.api(`/drives/${driveId}/root:/General/${fileName}:/createUploadSession`)
+        .post({
+          item: {
+            '@microsoft.graph.conflictBehavior': 'rename',
+            name: fileName,
+          },
+        });
+   
+      const uploadUrl = uploadSession.uploadUrl;
+      const maxChunkSize = 320 * 1024; // 320 KB
+      let start = 0;
+      let end = maxChunkSize;
+      let fileSlice;
+     
+      while (start < fileSize) {
+        if (fileSize - end < 0) {
+          end = fileSize;
+        }
+      
+        fileSlice = Buffer.alloc(maxChunkSize);
+        const bytesRead = fileContent.read(fileSlice, 0, maxChunkSize, start);
+      
+        const response = await fetch(uploadUrl, {
+          method: 'PUT',
+          headers: {
+            'Content-Range': `bytes ${start}-${start + bytesRead - 1}/${fileSize}`,
+          },
+          body: fileSlice.slice(0, bytesRead),
+        });
+      
+        start += bytesRead;
+        end += maxChunkSize;
+      }
+
+      //Complete the upload
+      const response = await fetch(uploadUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Length': 0,
+        },
+      });
+
+      //Get the URL to the uploaded file
+
+
+      const deepLink = `https://teams.microsoft.com/l/file/${encodeURIComponent(fileName)}/preview?groupId=${teamId}&tenantId=${appAuthConfig.tenantId}&channelId=${generalChannelId}`;
+      const uploadedFileUrl = `https://teams.microsoft.com/_#/files/tab/${teamId}/${driveId}/${encodeURIComponent(fileName)}`;
+      console.log(`File uploaded successfully to ${uploadedFileUrl}`);
+      return deepLink;
+    } catch (error) {
+      console.error(`Error uploading file: ${error}`);
+      throw error;
+    }
+  }
+
 };
